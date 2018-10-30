93f62d04fb40c526e6d7436033a6a9154e2ead6e
tgadf <tgadfort@gmail.com>
2018-10-29 20:47:59 -0500

    README

---
+++ setup.py
@@ -0,0 +1,35 @@
+import codecs
+from setuptools import setup
+
+
+def readme():
+    with codecs.open('README.md') as f:
+        return f.read()
+
+setup(
+    name='geohash',
+    version='0.0.1',
+    description='?',
+    long_description=readme(),
+    url='http://github.com/tgadf/geohash',
+    keywords='geohash',
+    author='Thomas Gadfort',
+    author_email='tgadfort@gmail.com',
+    classifiers=[ 
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    license='MIT',
+    install_requires=[],
+    packages=['geohash'],
+    scripts=[],
+    zip_safe=False,
+    include_package_data=True,
+    project_urls={  
+        'Bug Reports': 'https://github.com/tgadf/geohash/issues',
+        'Source': 'https://github.com/tgadf/geohash'
+    },
+)
+ 
